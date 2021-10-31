6ef9ebb4abd9a2d08eb7d13b65343d70e2e2e56f
karimdaw91 <karimdaw91@gmail.com>
2021-10-31 02:56:16 +0200

    fixed option B predict

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ import numpy as np
 import joblib
 import myMl
 
-
-
 # register hops app as middleware
 app = Flask(__name__)
 hops = hs.Hops(app)
@@ -18,25 +16,51 @@ hops = hs.Hops(app)
     "/getPrediction",
     name="GetCO2Prediction",
     description="Predicts the CO2 emission of chosen building type",
-    #icon="learncarbon_logo_without_text.png",
+    icon="learncarbon_logo_without_text.png",
     inputs=[
-        hs.HopsInteger("constructionType", "CT", "Input Choose construction type"),
-        hs.HopsInteger("buildingType", "BT", "Input Choose building type"),
-        hs.HopsInteger("location", "L", "Input Choose location of project"),
-        hs.HopsNumber("area", "A", "Input Total gross area of project"),
-        hs.HopsNumber("floorCount", "FC", "Input Floor Count")
+        hs.HopsInteger("constructionType", "ConstructionType", "Input Choose construction type"),
+        hs.HopsInteger("buildingType", "BuildingType", "Input Choose building type"),
+        hs.HopsInteger("location", "Location", "Input Choose location of project"),
+        hs.HopsNumber("area", "Area", "Input Total gross area of project"),
+        hs.HopsNumber("floorCount", "FloorCount", "Input Floor Count")
     ],
     outputs=[
-        hs.HopsNumber("Prediction", "P", "CO2 prediction")
+        hs.HopsNumber("C02_Prediction", "C02_Prediction", "CO2 prediction")
     ]
 )
-
-
 def getPrediction(constructionType,buildingType,location,area,floorCount):
     # fetch Prediction data from myML
     fetchedPrediction = myMl.RunPredictionOp1(constructionType,buildingType,location,area,floorCount)
+    print("prediction done!")
     print("fetched prediction data")
     return fetchedPrediction
+
+@hops.component(
+    "/getConstructionType",
+    name="GetSTRUCTUREPrediction",
+    description="Predicts the type of contruction given a target C02 emission",
+    icon="learncarbon_logo_without_text.png",
+    inputs=[
+        #hs.HopsBoolean("Run prediction", "RP", "Run the prediction"),
+        hs.HopsNumber("CO2", "TargetC02", "Choose target CO2 emission"),
+        hs.HopsInteger("buildingType_b", "BuildingType", "Input building type"),
+        hs.HopsInteger("location_b", "Location", "Input choose location pf building"),
+        hs.HopsNumber("area_b", "TotalArea", "Input total gross area"),
+        hs.HopsInteger("floorCount_b", "FloorCount", "Input floor count"),
+    ],
+    outputs=[
+        hs.HopsString("construction_type", "ConstructionType_Prediction", "Construction type prediction")
+    ]
+)
+def getConstructionType(CO2, buildingType_b, location_b, area_b, floorCount_b):
+    prediction = myMl.RunPredictionOp2(CO2, buildingType_b, location_b, area_b, floorCount_b)
+
+    construction_type = prediction
+
+    return construction_type
+
+
+
 
 
 if __name__ == "__main__":
--- myMl.py
+++ myMl.py
@@ -16,7 +16,6 @@ def RunPredictionOp1(constructionType,buildingType,location,area,floorCount):
     model = tf.keras.models.load_model('LearnModel_1.h5')
     model.summary()
 
-    
     # Input 1 - Building Concstruction Type
     # 0 - RC
     # 1 - Steel Concrete
@@ -57,20 +56,85 @@ def RunPredictionOp1(constructionType,buildingType,location,area,floorCount):
     numPrediction = float(y_scaled[0][0])
     #print(numPrediction)
 
-    pathOut = r"C:\Users\karimd\source\repos\AEC_Hackathon2021\Output\prediction.txt"
-    np.savetxt(pathOut, y_scaled)
+    #pathOut = r"C:\Users\karimd\source\repos\AEC_Hackathon2021\Output\prediction.txt"
+    #np.savetxt(pathOut, y_scaled)
 
     return numPrediction
-
-def RunPredictionOp2():
-    pass
-
-def TogglePredictionOption():
-    pass
 
 
 
 # function option 2
+
+def RunPredictionOp2(CO2,Type_B,Location_B,Area,Floors):
+
+    #Load saved model
+    model = tf.keras.models.load_model('LearnModel_2.h5')
+    model.summary()
+    
+    # Input 1 - CO2 Emissions Numeical input
+    #CO2 = 2
+
+    # Input 2 - building type is 0 or 1
+    #Type_B = 1
+
+    # Input 3 - location 0 - 5
+    #Location_B = 3
+
+    # Input 4 - Total floor area 
+    #Area = 100
+
+    # Input 5 - floorCount usually between 5 and 25
+    #Floors = 10
+
+    # Create 2d numpy array 
+    inputData = np.array([[CO2], [Type_B], [Location_B], [Area], [Floors]])
+
+    #pathInp = r"C:\Users\karimd\source\repos\AEC_Hackathon2021\query.txt"
+    #inputData = np.genfromtxt(pathInp)
+
+    #CHANGE THE FILE LOCATION!!
+    scalerB = joblib.load(r"C:\Users\karimd\source\repos\AEC_Hackathon2021\scalerx_B.pkl")
+
+    # reshaping model 
+    inputData_new = np.reshape(inputData, (1,5))
+    
+    x_scaled = scalerB.transform(inputData_new)
+
+    y_pred = model.predict(x_scaled)
+    
+    construction_type = y_pred.argmax(axis=1)
+    print("this is the construction type")
+    print(construction_type)
+    #NOT SURE IF THE FLOAT IS NECESSARY HERE
+    construction_type_unwarpped = construction_type[0]
+    print(construction_type_unwarpped)
+
+    # check which construction types each integer is
+    if construction_type_unwarpped == 0:
+        construction_type_string = "Concrete"
+
+    if construction_type_unwarpped == 1:
+        construction_type_string = "Steel-Concrete"
+    
+    if construction_type_unwarpped == 2:
+        construction_type_string = "Wood"
+
+    if construction_type_unwarpped == 3:
+        construction_type_string = "Wood-Hybrid"
+
+    print(construction_type_string)
+    print(Area)
+    print(Floors)
+
+    #print(numPrediction)
+    #pathOut = r"C:\Users\karimd\source\repos\AEC_Hackathon2021\Output\prediction.txt"
+    #np.savetxt(pathOut, construction_type)
+
+    return construction_type_string
+
+
+
+
 
 
 
