8a253afcb2f6e323139061cfda29902dc34b5a34
PanayotaPouliou <yotapouliou@gmail.com>
2021-11-03 17:49:53 +0100

    clean data sortening

--- Data/Clean_data.py
+++ Data/Clean_data.py
@@ -1,16 +1,9 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import csv
-import altair as alt
-import tensorflow as tf
-import joblib
-from pandas.core.indexing import check_bool_indexer
-import seaborn as sns
 
 # Read dataset csv with the material column
-df = pd.read_csv(r"Data-Set-Material-Categories.csv")
+df = pd.read_csv(r"C:\Users\ppou\src\Data\Data-Set-Material-Categories.csv")
+#print(df)
 
 # Function to convert binary columns based on their carbon emission
 def pick_value(column_feature, array_bounds, column_carbon):
@@ -32,20 +25,23 @@ def pick_value(column_feature, array_bounds, column_carbon):
   
 #--------------- Organizing data ---------------
 data = df[['Building Type', 'Location', 'Foot print (m2)', 'Floor Count', 'Life Time (Years)', 'TC02 (Normalized at 50 years)', 'Building_Construction_Type']]
+
 # Deleting rows with no values
-data = dframe.dropna()
-data = data.rename(columns={0 : 'Type', 1: 'Location', 2 : 'Area', 3 : 'Floors', 4 : 'Build_life', 5 : 'CO2', 6 : 'Building_Construction_Type'})
-data.head("Clear data: ", clearedDf.head())
+data = data.dropna()
+
+data = data.rename(columns={'Building Type' : 'Type', 'Location' : 'Location', 'Foot print (m2)' : 'Area', 'Floor Count' : 'Floors', 'Life Time (Years)' : 'Build_life', 'TC02 (Normalized at 50 years)' : 'CO2', 'Building_Construction_Type' : 'Building_Construction_Type'})
+#print(data)
+
 # Convert from numerical to categorical
-data.Type = pd.Categorical(clearedDf.Type)
+data.Type = pd.Categorical(data.Type)
 data['Type_B'] = data.Type.cat.codes
-data.Location = pd.Categorical(clearedDf.Location)
+data.Location = pd.Categorical(data.Location)
 data['Location_B'] = data.Location.cat.codes
-data.Area = pd.Categorical(clearedDf.Area)
+data.Area = pd.Categorical(data.Area)
 data['Area_B'] = data.Area.cat.codes
-data.Floors = pd.Categorical(clearedDf.Floors)
+data.Floors = pd.Categorical(data.Floors)
 data['Floors_B'] = data.Floors.cat.codes
-data.Building_Construction_Type = pd.Categorical(clearedDf.Building_Construction_Type)
+data.Building_Construction_Type = pd.Categorical(data.Building_Construction_Type)
 data['Building_Construction_Type'] = data.Building_Construction_Type.cat.codes
 
 # --------------- Convert the categorical columns (1 to 6 floors) to numerical based on the carbon footprint of the sample ---------------
@@ -54,17 +50,20 @@ footprint_bounds_cat = [18581, 46451, 2324, 4645, 46452, 92903, 4646, 9290, 466,
 floor_bounds_cat = [1, 6, 15, 25, 7, 14, 25, 50]
 
 # --------------- AREA ---------------
-column_c_np = np.asarray(CO2)
-column_f_np = np.asarray(Footprint_B)
+column_c_np = np.asarray(data['CO2'].values)
+column_f_np = np.asarray(data['Area_B'].values)
 test_area_mapped_fo = pick_value(column_f_np, footprint_bounds_cat, column_c_np)
-data['clear_Area'] = test_area_mapped
+data['clear_Area'] = test_area_mapped_fo
 
 # --------------- FLOORS ---------------
 column_floors = np.asarray(data['Floors_B'].values)
 test_area_mapped_fl = pick_value(column_floors, floor_bounds_cat, column_c_np)
 data['clear_floors'] = test_area_mapped_fl
 
+
+
 # --------------- Clean one more time ---------------
 data.drop(columns=['Type', 'Location', 'Area', 'Floors',  'Build_life', 'Area_B', 'Floors_B', 'Area_B'], inplace=True)
+
 # --------------- Save if needed ---------------
-# data.to_csv("/content/gdrive/MyDrive/temporary/final_data", index=False)
+#data.to_csv("Data/final_data.csv", index=False)
