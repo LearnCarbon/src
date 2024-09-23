9b4fc3064f02a2e7a30971884e1ddc67e269bc2c
Iliana Papadopoulou <iliana@reope.com>
2024-09-23 12:35:11 +0200

    fixed modelB and way to pass arguments

--- run_ml.py
+++ run_ml.py
@@ -6,47 +6,64 @@ import joblib
 import numpy as np
 # r: scikit-learn
 import sklearn
-
 import os
-# r: argparse
-import argparse
+import Rhino
+# r: dill
 
 print("NumPy version:", np.__version__)
-
-parser = argparse.ArgumentParser(description="Run machine learning model predictions")
-
-# Adding arguments for both modelA and modelB
-parser.add_argument("--targetCo2", type=float, help="Target CO2 for modelB")
-parser.add_argument("--constructionType", type=int, help="Construction type (0-3) for modelA")
-parser.add_argument("--buildingType", type=int, help="Building type (0 or 1) for modelA")
-parser.add_argument("--location", type=int, help="Location (0-5) for modelA")
-parser.add_argument("--area", type=float, help="Total floor area for modelA")
-parser.add_argument("--floorCount", type=int, help="Floor count (usually between 5 and 25) for modelA")
-
-# Parse arguments from the command line
-args = parser.parse_args()
 
 # region COnfihuring dirs
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Dynamically find paths based on the project directory
 log_dir = os.path.join(current_dir, 'log')
-model_path = os.path.join(log_dir, 'LearnModel_A.h5')
+modelA_path = os.path.join(log_dir, 'LearnModel_A.h5')
+modelB_path = os.path.join(log_dir, 'LearnModel_B.h5')
 scalerY_path = os.path.join(log_dir, 'scalerY_A.pkl')
 scalerX_path = os.path.join(log_dir, 'scalerx_A.pkl')
+scalerX_B_path = os.path.join(log_dir, 'scalerx_B.pkl')
 result_file_path = os.path.join(log_dir, 'prediction_result.txt')
+input_file_path = os.path.join(log_dir, 'input.txt')
 # endregion
-#Load saved model
-model = tf.keras.models.load_model(model_path)
 
-def modelB(targetCo2):
-    print(f"Processing with targetCo2: {targetCo2}")
+def modelB(targetCo2, buildingType, location, area, floorCount):
+    Rhino.RhinoApp.WriteLine(f"Processing with targetCo2: {targetCo2}")
+    model = tf.keras.models.load_model(modelB_path)
+    inputData = np.array([[targetCo2], [buildingType], [location], [area], [floorCount]])
+
+    scalerX = joblib.load(scalerX_B_path)
+
+    inputData_new = np.reshape(inputData, (1, 5))
+    x_scaled = scalerX.transform(inputData_new)
+
+    # Make prediction
+    prediction = model.predict(x_scaled)
+
+    construction_type = prediction.argmax(axis=1)
+    print("this is the construction type")
+    print(construction_type)
+    construction_type_unwarpped = construction_type[0]
+    print(construction_type_unwarpped)
+
+    # check which construction types each integer is
+    if construction_type_unwarpped == 0:
+        construction_type_string = "Concrete"
+
+    if construction_type_unwarpped == 1:
+        construction_type_string = "Steel-Concrete"
+    
+    if construction_type_unwarpped == 2:
+        construction_type_string = "Wood"
+
+    if construction_type_unwarpped == 3:
+        construction_type_string = "Wood-Hybrid"
 
     with open(result_file_path, 'w') as f:
-        f.write(str(targetCo2))
+        f.write(construction_type_string)
 
 # Function 2: Takes all arguments
 def modelA(constructionType, buildingType, location, area, floorCount):
+    model = tf.keras.models.load_model(modelA_path)
     # Prepare input data
     inputData = np.array([[constructionType], [buildingType], [location], [area], [floorCount]])
     
@@ -63,19 +80,19 @@ def modelA(constructionType, buildingType, location, area, floorCount):
     
     y_scaled = scalerY.inverse_transform(prediction)
     numPrediction = float(y_scaled[0][0])
-    
-    print("Building 1: " + str(numPrediction))
-    
+        
     # Write the result to file
     with open(result_file_path, 'w') as f:
         f.write(str(numPrediction))
+    
+# Read inputs from the input file
+with open(input_file_path, 'r') as f:
+    inputs = f.read().strip().split(',')
 
-if args.targetCo2 is not None:
-    # If targetCo2 is provided, run modelB
-    modelB(args.targetCo2)
-elif all([args.constructionType is not None, args.buildingType is not None, 
-          args.location is not None, args.area is not None, args.floorCount is not None]):
-    # If all the required arguments for modelA are provided, run modelA
-    modelA(args.constructionType, args.buildingType, args.location, args.area, args.floorCount)
+if inputs[0] == "False":
+    targetCo2, buildingType, location, area, floorCount = map(float, inputs[1:])
+    modelB(targetCo2, buildingType, location, area, floorCount)
 else:
-    print("Invalid arguments. Please provide either targetCo2 or all arguments for modelA.")
+    print(inputs)
+    constructionType, buildingType, location, area, floorCount = map(float, inputs[1:])
+    modelA(int(constructionType), int(buildingType), int(location), area, int(floorCount))
